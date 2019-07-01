8156d16615e204a0c8a6489638e98dd21a95254a
Cristofer <cristofer.sereno@xemantics.com>
2019-07-01 18:41:32 -0400

    Navegacion entre archivos

--- src/app/series/ciencia/ciencia.page.ts
+++ src/app/series/ciencia/ciencia.page.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, ActionSheetController } from '@ionic/angular';
+import { CrudService } from '../../services/crud.service';
+
+// Crear interface para array
+interface pelicula {
+  id: string;
+  categoria: string;
+  titulo: string;
+  genero: string;
+  pais: string;
+  sinopsis: string;
+  img: string;
+  clasificacion: number;
+}
 
 @Component({
   selector: 'app-ciencia',
@@ -7,9 +21,158 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CienciaPage implements OnInit {
 
-  constructor() { }
+  // Array para mostrar en HTML
+  peliculas: any = [];
+
+  // Variable para estrellas
+  clasificacion: any;
+
+  // Variables para funcionalidad
+  ngMTipo: boolean;
+  mostrarTg: boolean;
+  uno: any = false;
+  dos: any = false;
+  tres: any = false;
+  cuatro: any = false;
+  cinco: any = false;
+
+  constructor(
+    public navController: NavController,
+    public actionSheetController: ActionSheetController,
+    private crudService: CrudService
+  ) { }
 
   ngOnInit() {
+
+    // Cargar datos desde Firebase
+    this.crudService.getPelicula('accion').subscribe(peliculas => {
+      peliculas.map(pelicula => {
+        //console.log(pelicula.payload.doc.data());
+        const data: pelicula = pelicula.payload.doc.data() as pelicula;
+        data.id = pelicula.payload.doc.id;
+        //console.log(data);
+        this.peliculas.push(data);
+        // Elegir clasificación
+        switch (data.clasificacion) {
+          case 5:
+            this.cinco = true;
+            this.verEstrellas5();
+            break;
+          case 4:
+            this.cuatro = true;
+            this.verEstrellas4();
+            break;
+          case 3:
+            this.tres = true;
+            this.verEstrellas3();
+            break;
+          case 2:
+            this.dos = true;
+            this.verEstrellas2();
+            break;
+          case 1:
+            this.uno = true;
+            this.verEstrellas1();
+            break;
+          default:
+            this.uno = false;
+            this.verEstrellas1();
+            break;
+        }
+      });
+    });
+
+  }
+
+  volver() {
+    this.navController.back();
+  }
+
+  verTg() {
+    if(!this.ngMTipo) {
+      this.mostrarTg = true;
+    } else {
+      this.mostrarTg = false;
+    }
+  }
+
+  async compartir() {
+    const share = await this.actionSheetController.create({
+      header: 'Opciones',
+      buttons:
+      [
+        {
+          text: 'Facebook',
+          icon: 'logo-facebook'
+        },
+        {
+          text: 'Pinterest',
+          icon: 'logo-pinterest'
+        },
+        {
+          text: 'Tumblr',
+          icon: 'logo-tumblr'
+        },
+        {
+          text: 'Twitter',
+          icon: 'logo-twitter'
+        }
+      ]
+    });
+    share.present();
+  }
+
+  verEstrellas5() {
+    console.log('Ver estrellas 5');
+    if (this.cinco) {
+      this.cuatro = true;
+      this.tres = true;
+      this.dos = true;
+      this.uno = true;
+    }
+  }
+
+  verEstrellas4() {
+    console.log('Ver estrellas 4');
+    if (this.cuatro) {
+      this.tres = true;
+      this.dos = true;
+      this.uno = true;
+    } else {
+      this.cinco = false;
+    }
+  }
+
+  verEstrellas3() {
+    console.log('Ver estrellas 3');
+    if (this.tres) {
+      this.dos = true;
+      this.uno = true;
+    } else {
+      this.cinco = false;
+      this.cuatro = false;
+    }
+  }
+
+  verEstrellas2() {
+    console.log('Ver estrellas 2');
+    if (this.dos) {
+      this.uno = true;
+    } else {
+      this.cinco = false;
+      this.cuatro = false;
+      this.tres = false;
+    }
+  }
+
+  verEstrellas1() {
+    console.log('Ver estrellas 1');
+    if (!this.uno) {
+      this.cinco = false;
+      this.cuatro = false;
+      this.tres = false;
+      this.dos = false;
+    }
   }
 
 }
